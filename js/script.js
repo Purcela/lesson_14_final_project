056f4b0181fd35747d5b8055da6cf0038f7608df
Purcela <purceladzeg@gmail.com>
2022-03-30 18:52:26 +0400

    add footer ; about ; menu.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 $("#burger-menu").on("click", function(){
     $("#menu").toggleClass("open-menu");
   })
+  $("#burger-menu1").on("click", function(){
+    $("#menu1").toggleClass("open-menu");
+  })
