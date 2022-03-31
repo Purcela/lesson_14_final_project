42c08c6d141e17e18fcc8f8be44c4d8898554fc0
Purcela <purceladzeg@gmail.com>
2022-03-31 19:36:42 +0400

    add gallery; start contact

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 $("#burger-menu").on("click", function(){
     $("#menu").toggleClass("open-menu");
   })
-  $("#burger-menu1").on("click", function(){
-    $("#menu1").toggleClass("open-menu");
-  })
+$("#burger-menu1").on("click", function(){
+  $("#menu1").toggleClass("open-menu");
+})
+$("#gallery-menu").on("click", function(){
+  $("#menu2").toggleClass("open-menu");
+})
